306730a5395f49fcc6bd69b20e21632c83358667
Ian Sweeney <173560+spr12ian@users.noreply.github.com>
2020-05-16 22:52:15 +0100

    Order days from start of year

--- MenuRota.js
+++ MenuRota.js
@@ -21,15 +21,57 @@ class MenuRota {
     return sheetName;
   }
 
-  getSheet() { //console.log("DailyMenus.getSheet");  
+  getSheet() {
     const sheet = this.mySpreadsheet.getActiveSpreadsheet().getSheetByName(this.getSheetName()); //console.log(sheet);
     return sheet;
   }
   
   getOutput() {
+    
+    function getDaysArray() {
+      const date = getFirstDateOfYear();
+      const options = { weekday: 'long'};
+      const dtf = new Intl.DateTimeFormat('en-US', options);
+      const daysArray = [];
+      for (let i = 0; i < 7; i++) {
+        daysArray.push(dtf.format(date));
+        date.setDate(date.getDate() + 1);
+      }
+      console.log(daysArray);
+      return daysArray;
+    }
+    
+    function getFirstDateOfYear() {
+      const date = new Date();
+      date.setMonth(0,1);
+      
+      return date;
+    }
+    
+    function getFirstDayOfYear() {
+      const date = getFirstDateOfYear();
+      const options = { weekday: 'long'};
+      const dtf = new Intl.DateTimeFormat('en-US', options);
+      const day = dtf.format(date);
+      
+      return day;
+    }
+    
+    function makeDaysIterator() {
+      const date = getFirstDateOfYear();
+      const dtf = new Intl.DateTimeFormat('en-GB');
+
+      const nextIterator = {
+        next: function() {      
+          const result = { day: dtf.format(date) };
+          date.setDate(date.getDate()+1);    
+          return result;
+        }
+      };
+      return nextIterator;
+    }
   
     function* makeInfiniteIterator(arr) {
-      console.log("makeInfiniteIterator: arr = %s", arr);
       const start = 1;
       const end = arr.length;
       
@@ -58,58 +100,46 @@ class MenuRota {
         return meals;
       }
       
-      function getIteratorGrid() {
+      function getIteratorGrid(arr) {
         const iteratorGrid = {};
         let arrIndex = 0;
-        console.log("getIteratorGrid: daysArray = %s", daysArray);
-        daysArray.forEach(day => {
+        
+        const mealtimesArray = [
+          'Breakfast',
+          'Lunch',
+          'Dinner',
+          'Snacks',
+        ];
+      
+        getDaysArray().forEach(day => {
           iteratorGrid[day] = {};
           mealtimesArray.forEach(mealtime => {
             const meals = getMeals(arr[arrIndex++]);
             iteratorGrid[day][mealtime] = makeInfiniteIterator(meals);
-          
-            for (let mealIndex = 0; mealIndex <= meals.length; mealIndex++) {
-              console.log(iteratorGrid[day][mealtime].next());
-            }
           });
         });
         
         return iteratorGrid;
       }
       
-      console.log("makeMealsIterator: arr = %s", arr);
-      let start = 1
+      if (getFirstDayOfYear() !== arr[0][0]) {
+        throw new Error("Menu Rota should start on a " + getFirstDayOfYear() + " not on a " + arr[0][0]);
+      }
+      
+      const iteratorGrid = getIteratorGrid(arr);
+      
+      const start = 1;
+      
       let nextIndex = start;
       let end = arr.length;
-      console.log("makeMealsIterator: end = %s", end);
-      
-      const iteratorGrid = getIteratorGrid();
-      console.log("makeMealsIterator: iteratorGrid = %s", iteratorGrid);
-      
-      console.log("makeMealsIterator: iteratorGrid has %s keys", Object.keys(iteratorGrid).length);
-      Object.keys(iteratorGrid).forEach(day => {
-        console.log("makeMealsIterator: day = %s", day);
-        console.log("makeMealsIterator: iteratorGrid["+day+"] has %s keys", Object.keys(iteratorGrid[day]).length);
-        Object.keys(iteratorGrid[day]).forEach(mealtime => {
-          console.log("makeMealsIterator: mealtime = %s", mealtime);
-        });
-      });
 
-      const rangeIterator = {
-        next: function() {
-          console.log("rangeIterator: nextIndex = %s", nextIndex);
-          
-          const day = arr[nextIndex-1][0];
-          console.log("rangeIterator: day = %s", day);
-          
-          const mealtime = arr[nextIndex-1][1];
-          console.log("rangeIterator: mealtime = %s", mealtime);
-          
+      const nextIterator = {
+        next: function() {          
+          const day = arr[nextIndex-1][0];          
+          const mealtime = arr[nextIndex-1][1];          
           const meal = iteratorGrid[day][mealtime].next().value;
-          console.log("rangeIterator: meal = %s", meal);
           
           let result = { day: day, mealtime: mealtime, meal: meal };
-          console.log("rangeIterator: result = %s", result);
           
           if (nextIndex < end) {
             nextIndex++;
@@ -119,64 +149,33 @@ class MenuRota {
           return result;
         }
       };
-      return rangeIterator;
+      return nextIterator;
     }
-      
-    const daysArray = [
-        'Sunday',
-        'Monday',
-        'Tuesday',
-        'Wednesday',
-        'Thursday',
-        'Friday',
-        'Saturday',
-      ];
-    console.log("getOutput: daysArray = %s", daysArray);
-    
-    const mealtimesArray = [
-      'Breakfast',
-      'Lunch',
-      'Dinner',
-      'Snacks',
-    ];
-    console.log("getOutput: mealtimesArray = %s", mealtimesArray);
-    
-    const days = makeInfiniteIterator(daysArray);
-    console.log("getOutput: days = %s", days);
-    
-    const mealtimes = makeInfiniteIterator(mealtimesArray);
-    console.log("getOutput: mealtimes = %s", mealtimes);
     
     const dataRangeValues = this.getDataRangeValues();
     // Get rid of the header
     dataRangeValues.shift();
-    console.log("getOutput: dataRangeValues = %s", dataRangeValues);
     
+    const days = makeDaysIterator();
     const meals = makeMealsIterator(dataRangeValues);
-    console.log("getOutput: meals = %s", meals);
     
     const output = [];
+    
     output.push(this.getDailyMenusHeader());
     
     const daysInYear = 366;
-    console.log("getOutput: daysInYear = %s", daysInYear);
-    
-    for (let dayIndex = 0; dayIndex < daysInYear; dayIndex++) {
-      console.log("getOutput: dayIndex = %s", dayIndex);
+  
+    for (let dayIndex = 0; dayIndex < daysInYear; dayIndex++) {      
+      const day = days.next().day;
       
-      const next = meals.next();
-      console.log("getOutput: next = %s", next);
-      
-      const day = next.day;
-      
-      const breakfast = next.meal;
+      const breakfast = meals.next().meal;
       const lunch = meals.next().meal;
       const dinner = meals.next().meal;
       const eveningSnack = meals.next().meal;
       
-      const row = [day, breakfast, lunch, dinner, eveningSnack];
-      output.push(row);
+      output.push([day, breakfast, lunch, dinner, eveningSnack]);
     }
+    
     return output;
   }
   
